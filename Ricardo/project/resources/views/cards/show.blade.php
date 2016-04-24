51d72ae575615e264303f912878cead329b4cfb6
RicardoTpsip <ricardo.rodrigues.t0100534@edu.atec.pt>
2016-04-24 17:11:44 +0100

    eloquents
    ligações entre tabelas

---
+++ Ricardo/project/resources/views/cards/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+
+<h1>
+    {{$card->title}}
+    
+</h1>
+<ul>
+@foreach ($card->notes as $note)
+
+<li>{{$note->body}}</li>
+
+@endforeach
+</ul>
+@stop
